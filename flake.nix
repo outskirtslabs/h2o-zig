88836b65b3a26c53af34fa42ec6055b01b5dcce4
Casey Link <casey@outskirtslabs.com>
2025-10-20 14:25:27 +0200

    simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "dev env for h2o";
+  description = "dev env for h2o zig";
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
     flakelight.url = "github:nix-community/flakelight";
@@ -45,55 +45,14 @@
         let
           zigpkgs = zig.packages.${pkgs.system};
           apple-sdk = (self.packages.${pkgs.system}.apple-sdk);
-          nativeBinSubdir = "zig-out/${builtins.replaceStrings [ "darwin" ] [ "macos" ] pkgs.system}";
-          libraries = [
-          ];
         in
         {
-          #nativeBuildInputs = [
-          #];
           packages = [
-            # H2O build dependencies from it's package.nix
-            pkgs.curl
             pkgs.git
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.pkg-config
-            pkgs.makeWrapper
-            pkgs.brotli
-            pkgs.openssl
-            pkgs.libcap
-            pkgs.libuv
             pkgs.perl
-            pkgs.zlib
-            pkgs.wslay
-            pkgs.bison
-            pkgs.ruby
-            pkgs.liburing
-
-            # Development tools
             zigpkgs."0.15.2"
-            pkgs.go
-            pkgs.gdb
-            pkgs.clojure-lsp
-            pkgs.jdt-language-server
-            pkgs.clang-tools
-            pkgs.clj-kondo
-            pkgs.cljfmt
-            pkgs.babashka
-            pkgs.git
-            apple-sdk
-            pkgs.nodejs
-            pkgs.corepack
-            pkgs.nodePackages.typescript
-            #pkgs.nghttp2 # for h2load
-            #pkgs.clang
-            #pkgs.llvmPackages.clangUseLLVM
           ];
-          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
           env.APPLE_SDK_PATH = "${apple-sdk}";
-          env.MACOSX_SDK_SYSROOT = "${apple-sdk}";
-          #env.PATH = "$PWD/${nativeBinSubdir}:$PATH";
           env.ZIG_GLOBAL_CACHE_DIR = ".zig-cache-global";
         };
       flakelight.builtinFormatters = false;
